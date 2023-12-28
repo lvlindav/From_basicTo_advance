691b6ac30f734f5bcfe416256be968292b37c88a
lvlindav <72899964+lvlindav@users.noreply.github.com>
2023-12-27 21:02:30 -0600

    way of contact

--- skills.js
+++ skills.js
@@ -8,3 +8,26 @@ $(document).ready(function(){
       });
     });
   });
+
+  function loadRepo() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        results = JSON.parse(this.responseText);
+        for (let i = 0; i < results.length; i++) {
+          console.log(results[i].name);
+          let ul = document.getElementById("repos");
+          let li = document.createElement("LI");
+          li.className = "list-group-item";
+          let a = document.createElement("a");
+  
+          li.appendChild(document.createTextNode(results[i].name));
+          a.appendChild(li);
+          a.setAttribute("href", results[i].html_url);
+          ul.appendChild(a);
+        }
+      }
+    };
+    xhttp.open("GET", "https://api.github.com/users/lvlindav/repos", true);
+    xhttp.send();
+  }
